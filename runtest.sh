e8b4db98fc7ac2dc3ad12401eddb16a625ebe54c
Kit Fung <sa9510@gmail.com>
2016-04-21 10:27:42 +0800

    Let the script able to get parameter

--- runtest.sh
+++ runtest.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
-allparams="$1"
-chrome_driver_loc="$2"
-chrome_loc="$3"
+chrome_driver_loc="$1"
+chrome_loc="$2"
+allparams="$3"
+
+paramsstr=""
+for param in $allparams
+do
+    KEY="${param%%:*}"
+    VALUE="${param##*:}"
+    paramsstr+="-D${KEY}=${VALUE} "
+done
 
 # run
 cp config.properties.etc config.properties
 mvn -Dmaven.test.skip=true clean install
-mvn -Dwebdriver.chrome.bin=$chrome_loc -Dwebdriver.chrome.driver=$chrome_driver_loc -Dparams=$allparams -Dtest=ProductionSmokeTest test
+mvn -Dwebdriver.chrome.bin=$chrome_loc -Dwebdriver.chrome.driver=$chrome_driver_loc -Dtest=ProductionSmokeTest $paramsstr test
 
